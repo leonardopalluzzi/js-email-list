9341d7dcc579690f20c7cb428755556f0fc2223f
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-27 16:10:08 +0100

    add generate 10 random emails and print on page

--- main.js
+++ main.js
@@ -2,12 +2,17 @@
 const emailListEl = document.querySelector('.email_list');
 const btnEl = document.querySelector('.btn');
 
-fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-    //recover API content promis
-    .then(response => response.json()) 
-    
-    //pars json obj promis
-    .then(data => {
-        console.log(data.response);
-    })
-    .catch(error => console.error(error))
+
+for(let i = 0; i < 9; i++){
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        //recover API content promis
+        .then(response => response.json()) 
+        
+        //pars json obj promis
+        .then(email => {
+            const markup = `<li>${email.response}</li>`
+            emailListEl.innerHTML += markup
+        })
+        .catch(error => console.error(error));
+}
+   
