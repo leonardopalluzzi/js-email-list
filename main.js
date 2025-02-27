2190695b96cae14c8fb393783b85209e33d1936b
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-27 16:17:06 +0100

    add regenerate email on button click

--- main.js
+++ main.js
@@ -3,7 +3,23 @@ const emailListEl = document.querySelector('.email_list');
 const btnEl = document.querySelector('.btn');
 
 
-for(let i = 0; i < 9; i++){
+// for(let i = 0; i < 9; i++){
+//     fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+//         //recover API content promis
+//         .then(response => response.json()) 
+        
+//         //pars json obj promis
+//         .then(email => {
+//             const markup = `<li>${email.response}</li>`
+//             emailListEl.innerHTML += markup
+//         })
+//         .catch(error => console.error(error));
+//     console.log(emailListEl);
+// }
+
+
+function getEmail (list){
+    list.innerHTML = '';
     fetch('https://flynn.boolean.careers/exercises/api/random/mail')
         //recover API content promis
         .then(response => response.json()) 
@@ -11,8 +27,20 @@ for(let i = 0; i < 9; i++){
         //pars json obj promis
         .then(email => {
             const markup = `<li>${email.response}</li>`
-            emailListEl.innerHTML += markup
+            list.innerHTML += markup
         })
         .catch(error => console.error(error));
+    return list
 }
-   
+
+console.log(getEmail(emailListEl));
+
+for(let i = 0; i < 9; i++){
+    getEmail(emailListEl);
+}
+
+btnEl.addEventListener('click', () => {
+    for(let i = 0; i < 9; i++){
+        getEmail(emailListEl);
+    }
+})
