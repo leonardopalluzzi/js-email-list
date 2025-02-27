c284cec8a31e4a9b542f77b03a960b4b63ec4f67
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-27 15:42:26 +0100

    add AJAX call to boolean API

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // select elements from dom
 const emailListEl = document.querySelector('.email_list');
 const btnEl = document.querySelector('.btn');
+
+fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    //recover API content promis
+    .then(response => response.json()) 
+    
+    //pars json obj promis
+    .then(data => {
+        console.log(data.response);
+    })
+    .catch(error => console.error(error))
